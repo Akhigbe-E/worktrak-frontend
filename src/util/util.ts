2b119ccb10a4ba497e6e5b358d6c08dfca36d162
Emmanuel <akhigbemmanuel@gmail.com>
2020-07-31 16:09:11 +0100

    Add authentication logic

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,17 @@
+import jwt from "jsonwebtoken";
+
 export const isAuthenticated = (): boolean => {
   const token = window.localStorage.getItem("token") || "";
-  return !!token;
+  if (!!token) {
+    const secret = process.env.REACT_APP_SECRET || "";
+    jwt.verify(token, secret, function (err, decoded) {
+      if (err) {
+        window.localStorage.removeItem("token");
+        return false;
+      }
+      const { email, id } = (decoded as any).data;
+      return true;
+    });
+  }
+  return false;
 };
