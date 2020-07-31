948264df8d1f49e2b6a9572ebb11da9f6e7092e3
Emmanuel <akhigbemmanuel@gmail.com>
2020-07-31 01:41:19 +0100

    Basic Route component setup

---
+++ src/components/Routes/Routes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const Routes: React.FC = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default Routes;
