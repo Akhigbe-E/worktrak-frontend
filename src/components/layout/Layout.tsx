17a87d6c2de9dd4a2530c428e3bf173a5a7dc4a4
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-02 01:14:08 +0100

    Restyle layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,8 +9,16 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <SideBar />
-      <div className="pt-12"></div>
-      <div className=" w-full mx-auto">{children}</div>
+      {/* <div className="pt-12"></div> */}
+      <div
+        className="bg-customBlue-200 pt-12 pr-4 w-full"
+        style={{
+          paddingLeft: "17rem",
+          minHeight: "100vh",
+        }}
+      >
+        {children}
+      </div>
     </>
   );
 };
