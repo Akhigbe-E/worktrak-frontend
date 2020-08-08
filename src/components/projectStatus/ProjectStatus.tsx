b8163c4968fcdc2ecfb11c0b0d2ab7a45ce12b96
Emmanuel <akhigbemmanuel@gmail.com>
2020-08-08 14:31:17 +0100

    Make project status

--- src/components/projectStatus/ProjectStatus.tsx
+++ src/components/projectStatus/ProjectStatus.tsx
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProjectType } from "../dashboard/Dashboard";
+import { handleEditProjectClick } from "./projectStatusFunctions";
 
 export interface ProjectStatusPropType {
   project: ProjectType;
 }
 
+const renderStatusText = (status: string) => {
+  switch (status) {
+    case "on track":
+      return (
+        <span className="">
+          <span role="img" aria-label="celebrate">
+            🥂
+          </span>
+          We're on track{" "}
+        </span>
+      );
+    case "at risk":
+      return (
+        <span className="">
+          <span role="img" aria-label="celebrate">
+            ⚠
+          </span>
+          We're at risk
+        </span>
+      );
+    case "off track":
+      return (
+        <span className="">
+          <span role="img" aria-label="celebrate">
+            👎🏾
+          </span>
+          We're off track
+        </span>
+      );
+    default:
+      break;
+  }
+};
 const ProjectStatus: React.FC<ProjectStatusPropType> = () => {
-  return <div></div>;
+  const [inputtedStatus, setInputtedStatus] = useState("on track");
+  return (
+    <div
+      className="px-4 bg-customBlue-100 rounded-lg h-full"
+      style={{ minWidth: `${window.innerWidth - 290}px` }}
+    >
+      <div
+        className="bg-customBlue-120 mx-auto px-4 pt-3"
+        style={{ minHeight: `${window.innerHeight - 190}px` }}
+      >
+        <div className="flex">
+          <h4 className="font-bold text-white">
+            {renderStatusText(inputtedStatus)}
+          </h4>
+          <select
+            className="w-3/12 border border-gray-700 rounded-lg p-2"
+            onChange={(e) => {
+              handleEditProjectClick(e.target.value);
+            }}
+          >
+            <option selected={true} hidden={true} disabled={true}>
+              Select project status
+            </option>
+            <option value="on track">on track</option>
+            <option value="at risk">at risk</option>
+            <option value="off track">off track</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectStatus;
---
+++ src/components/projectStatus/projectStatusFunctions.ts
@@ -0,0 +1,1 @@
+export const handleEditProjectClick = (status: string) => {};
